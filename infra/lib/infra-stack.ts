e5e4e85cfe48387fb34be79a18407cbfcf6a8c1f
Sergio Laureano <sergiolaureano98@gmail.com>
2024-04-28 07:48:15 -0500

    Created IAM Role for Lambda access S3 bucket.

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,6 +11,12 @@ export class InfraStack extends cdk.Stack {
       bucketName: 'balancestatus-0123'
     });
     
+    const iamBalanceStatusRole = new iam.Role(this, 'iambalancerole', {
+      roleName: 'bankingLambdaRole',
+      description: 'Role for Lambda to access S3 bucket',
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'), // reference to the service the IAM role is used for
+    });
+    iamBalanceStatusRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonS3FullAccess'));
 
   }
 }
